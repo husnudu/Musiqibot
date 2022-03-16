ab92eff6a74e700f89064b17a4de9f23c8c2ce95
husnudu <99674788+husnudu@users.noreply.github.com>
2022-03-16 23:57:52 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
     API_ID = "13223132"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
     API_HASH = "123120ee6712a97e0e413425b32cee48" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "Bot token yapıştır" # 🤖 Get it from @botfather with /newbot command
+    BOT_TOKEN = "5174454348:AAGkcP078Cn128rl8xf02ZH5xbRRVFhIWS8" # 🤖 Get it from @botfather with /newbot command
