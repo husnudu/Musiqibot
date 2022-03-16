782366af627f227628f4d394ca70b9203ffe302e
husnudu <99674788+husnudu@users.noreply.github.com>
2022-03-16 23:53:17 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    API_ID = "App id yapışdır"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "api hash yapştır" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "Bot token yapıştır" # 🤖 Get it from @botfather with /newbot command
